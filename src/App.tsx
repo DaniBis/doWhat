ec54de37fee10922d35e342dcaacd840ed873803
DaniBis <bisceanudaniel@gmail.com>
2023-09-13 18:37:06 +0700

    adding login & signup form first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { useState } from 'react';
 import './App.css';
 import MenuBar from './navbar/menu';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/Login';
 
 function App() {
 
   return (
     <>   
-     <MenuBar />
+    <MenuBar />
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="max-w-md w-full space-y-8">
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </div>
     </>
   )
 }
