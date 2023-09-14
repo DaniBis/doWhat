7cf14e661f1ae2c2702da67e3182aa64a0c9afb7
DaniBis <bisceanudaniel@gmail.com>
2023-09-14 22:28:38 +0700

    setting up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import { useState } from 'react';
 import './App.css';
 import MenuBar from './navbar/menu';
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/Login';
+import Activities from './pages/Activities';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import SignupPage from './pages/Signup';
-import LoginPage from './pages/Login';
 
 function App() {
 
   return (
     <>   
-    <MenuBar />
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LoginPage/>} />
-            <Route path="/signup" element={<SignupPage/>} />
-        </Routes>
+      <BrowserRouter>
+      <MenuBar />
+      <Routes>
+          <Route path="/" element={<Activities/>} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/signup" element={<SignupPage/>} />
+      </Routes>
       </BrowserRouter>
-    </div>
-  </div>
     </>
   )
 }
