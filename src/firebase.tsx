2e592cb7dfb029b03e86f29763289647996c1b9e
DaniBis <bisceanudaniel@gmail.com>
2023-10-04 20:37:11 +0700

    adding images from storage to the collection

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcSjaEKZq6MV-qpzuBqQlSAOV1YFKARBg",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import { useQuery } from 'react-query';
 import { db } from '../firebase';
 import { collection, getDocs } from 'firebase/firestore';
@@ -11,6 +12,33 @@ const fetchActivities = async () => {
 
 const Activities: React.FC = () => {
   const { data: activities, isLoading } = useQuery('activities', fetchActivities);
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchImageUrls = async () => {
+      if (activities) {
+        const storage = getStorage();
+        const imageUrlsArray: string[] = [];
+
+        try {
+          for (const activity of activities) {
+
+            const imageName = `${activity.name}.png`; 
+            const imageRef = ref(storage, `${imageName}`);
+
+            const imageUrl = await getDownloadURL(imageRef);
+            imageUrlsArray.push(imageUrl);
+          }
+
+          setImageUrls(imageUrlsArray);
+        } catch (error) {
+          console.error('Error fetching image URLs:', error);
+        }
+      }
+    };
+
+    fetchImageUrls();
+  }, [activities]);
 
   return (
     <div className="bg-gray-100 p-4">
@@ -20,12 +48,15 @@ const Activities: React.FC = () => {
 
       {!isLoading && (
         <div>
-          {activities?.map((activity: any) => (
+          {activities?.map((activity: any, activityIndex: number) => (
             <div key={activity.name} className="bg-white p-4 mb-4 shadow-md rounded-md">
-              <div className="text-xl font-semibold">{activity.img}</div>
               <h2 className="text-xl font-semibold">{activity.name}</h2>
               <p className="text-gray-700">{activity.description}</p>
-              {/* Add more activity details */}
+              {imageUrls[activityIndex] && (
+                <div className="image-item">
+                  <img src={imageUrls[activityIndex]} alt={`Image ${activityIndex}`} />
+                </div>
+              )}
             </div>
           ))}
         </div>
